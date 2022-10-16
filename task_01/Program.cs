6ca3d6283d8be9c62fefebb4a58aa943e0460a60
Orange <dunduk201@yandex.ru>
2022-10-16 22:35:37 +0900

    task_01 was completed

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+
+int max = numberA;
+int min = numberB;
+
+if (numberA < numberB)
+{
+    max = numberB;
+    min = numberA;
+}
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
