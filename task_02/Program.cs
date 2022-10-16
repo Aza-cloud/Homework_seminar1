c18f0610552dddd6ebb8180f9ba4659bbd416ae5
Orange <dunduk201@yandex.ru>
2022-10-16 22:46:07 +0900

    task_02 was completed

---
+++ task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
